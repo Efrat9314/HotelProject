199359fa4eaa01c16ef83b3250f06ac785fcfba9
efrat9314 <efrat9314@gmail.com>
2023-11-15 17:59:16 +0200

    forth change

---
+++ HotelProject/HotelProject/Entities/DataContex.cs
@@ -0,0 +1,20 @@
+﻿namespace HotelProject.Entities
+{
+    public class DataContex
+    {
+        public List<Customer> customerList { get; set; }
+        public List<Room> roomList { get; set; }
+        public List<Order> orderList { get; set; }
+        public int [] roomNumber { get; set; }
+        public int orderNum { get; set; }
+
+        public DataContex()
+        {
+            customerList = new List<Customer>();
+            roomList = new List<Room>();
+            orderList = new List<Order>();
+            roomNumber = new int[8];
+            orderNum = 1;
+        }
+    }
+}
